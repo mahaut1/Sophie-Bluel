762ae02a8b6b54bddf917d78577cc2324f5d8f19
Mahaut Windal <mwindal@hotmail.com>
2024-01-20 18:24:29 +0100

    derniers réglages

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,22 +1,4 @@
 
-/***********************************************
-* INTERFACE ADMIN
-***********************************************/
-// on crée la barre noire de l'espace admin
-document.querySelector('.editMode').innerHTML=`
-<svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M13.5229 1.68576L13.8939 2.05679C14.1821 2.34503 14.1821 2.81113 13.8939 3.0963L13.0016 3.99169L11.5879 2.57808L12.4803 1.68576C12.7685 1.39751 13.2346 1.39751 13.5198 1.68576H13.5229ZM6.43332 7.73578L10.5484 3.61759L11.9621 5.03121L7.84387 9.14633C7.75494 9.23525 7.64455 9.29964 7.52496 9.33337L5.73111 9.84546L6.2432 8.05162C6.27693 7.93203 6.34133 7.82164 6.43025 7.73271L6.43332 7.73578ZM11.4408 0.646245L5.39074 6.6932C5.12397 6.95998 4.93078 7.28808 4.82959 7.64685L3.9526 10.7133C3.879 10.9708 3.94953 11.2468 4.13965 11.4369C4.32977 11.627 4.60574 11.6976 4.86332 11.624L7.92973 10.747C8.29156 10.6427 8.61967 10.4495 8.88338 10.1858L14.9334 4.13888C15.7951 3.27722 15.7951 1.87894 14.9334 1.01728L14.5624 0.646245C13.7007 -0.215415 12.3024 -0.215415 11.4408 0.646245ZM2.69844 1.84214C1.20816 1.84214 0 3.05031 0 4.54058V12.8812C0 14.3715 1.20816 15.5796 2.69844 15.5796H11.0391C12.5293 15.5796 13.7375 14.3715 13.7375 12.8812V9.44683C13.7375 9.039 13.4094 8.71089 13.0016 8.71089C12.5937 8.71089 12.2656 9.039 12.2656 9.44683V12.8812C12.2656 13.5589 11.7167 14.1078 11.0391 14.1078H2.69844C2.02076 14.1078 1.47188 13.5589 1.47188 12.8812V4.54058C1.47188 3.86291 2.02076 3.31402 2.69844 3.31402H6.13281C6.54065 3.31402 6.86875 2.98591 6.86875 2.57808C6.86875 2.17025 6.54065 1.84214 6.13281 1.84214H2.69844Z" fill="white"/>
-</svg>
-<p>Mode édition</p>
-`
-// on crée le stylo et le texte modifier
-document.querySelector('.btnModifyWorks').innerHTML=`
-<span><i class="fa-regular fa-pen-to-square"></i></span>
-<p>modifier</p>
-`
-// création d'une classe pour encapsuler le comportement lié à la connexion et à la déconnexion de l'administrateur
-
-
 /************************************************
 * GESTION DE LA MODALE
 *************************************************/
@@ -164,94 +146,91 @@ const btnCloseModal = document.querySelector('.fa-xmark');
 btnCloseModal.addEventListener('click', () => {
   closeModal();
 });
-
 // La classe ImageUploadForm() gère la logique du formulaire d'ajout d'un work. Elle prend en charge l'aperçu de l'image sélectionnée, la vérification des inputs, et la soumission du formulaire. Les éléments du formulaire, tels que le champ de téléchargement d'image, le titre, la catégorie, etc., sont manipulés ici.
 class ImageUploadForm {
-constructor() {
-  // Le constructeur est la méthode appelé lorsqu'on crée une instance de la classe. On sélectionne dans ce constructeur les différents éléments du formulaire et on les enregistre en tant qu'attribut pour pouvoir les manipuler par la suite.
-  this.uploadInput = document.getElementById("file-upload");
-  this.imagePreview = document.getElementById("image-preview");
-  this.titleInput = document.getElementById("titre");
-  this.addWorkForm = document.querySelector("#add-work-form");
-  this.categoryInput = document.getElementById("catégorie");
-  this.imageInput = document.getElementById("file-upload");
-  this.validateButton = document.getElementById("btn-validate");
-  this.faImg = document.querySelector(".fa-image");
-  this.addPicture = document.querySelector("#add-pic");
-  this.formatImage = document.querySelector("#format-image");
-  this.arrow= document.querySelector("#arrow")
- // on attache des écouteurs d'événements sur les inputs qui se déclenche quand l'admin saisie qqch et on appelle la méthode checkInputs pour vérifier la validité de l'input ou du formulaire en entier
-  this.uploadInput.onchange = this.updateImagePreview.bind(this);
-  this.titleInput.addEventListener("input", this.checkInputs.bind(this));
-  this.categoryInput.addEventListener("input", this.checkInputs.bind(this));
-  this.imageInput.addEventListener("input", this.checkInputs.bind(this));
-  this.addWorkForm.addEventListener("submit", this.handleSubmit.bind(this));
-}
-
-// On mets à jour l'aperçu de l'image et on enlève l'affichage des éléments de l'espace gris pour la remplacer par la photo choisie
-updateImagePreview() {
-  const image = new FileReader();
-  image.onload = (e) => {
-    this.imagePreview.src = e.target.result;
-    this.faImg.style.display = "none";
-    this.addPicture.style.display = "none";
-    this.formatImage.style.display = "none";
-    this.imagePreview.style.display = "flex";
-  };
-  image.readAsDataURL(this.uploadInput.files[0]);
-}
-
-// On vérifie la validité du formulaire et on change la couleur du bouton de validation de gris en vert si les données sont corrects
-checkInputs() {
-  if (this.titleInput.value && this.categoryInput.value && this.imageInput.value) {
-    this.validateButton.style.backgroundColor = "#1d6154";
-    this.validateButton.style.border = "#1d6154";
-    this.validateButton.style.color = "white";
-  } else {
-    this.validateButton.style.backgroundColor = "grey";
+  constructor() {
+    // Le constructeur est la méthode appelé lorsqu'on crée une instance de la classe. On sélectionne dans ce constructeur les différents éléments du formulaire et on les enregistre en tant qu'attribut pour pouvoir les manipuler par la suite.
+    this.uploadInput = document.getElementById("file-upload");
+    this.imagePreview = document.getElementById("image-preview");
+    this.titleInput = document.getElementById("titre");
+    this.addWorkForm = document.querySelector("#add-work-form");
+    this.categoryInput = document.getElementById("catégorie");
+    this.imageInput = document.getElementById("file-upload");
+    this.validateButton = document.getElementById("btn-validate");
+    this.faImg = document.querySelector(".fa-image");
+    this.addPicture = document.querySelector("#add-pic");
+    this.formatImage = document.querySelector("#format-image");
+   // on attache des écouteurs d'événements sur les inputs qui se déclenche quand l'admin saisie qqch et on appelle la méthode checkInputs pour vérifier la validité de l'input ou du formulaire en entier
+    this.uploadInput.onchange = this.updateImagePreview.bind(this);
+    this.titleInput.addEventListener("input", this.checkInputs.bind(this));
+    this.categoryInput.addEventListener("input", this.checkInputs.bind(this));
+    this.imageInput.addEventListener("input", this.checkInputs.bind(this));
+    this.addWorkForm.addEventListener("submit", this.handleSubmit.bind(this));
   }
-}
-
-// Gérer la soumission du formulaire et vérifier que le formulaire est valide
-async handleSubmit(event) {
-  event.preventDefault();
-  if (!this.isFormValid()) {
-    return;
+  
+  // On mets à jour l'aperçu de l'image et on enlève l'affichage des éléments de l'espace gris pour la remplacer par la photo choisie
+  updateImagePreview() {
+    const image = new FileReader();
+    image.onload = (e) => {
+      this.imagePreview.src = e.target.result;
+      this.faImg.style.display = "none";
+      this.addPicture.style.display = "none";
+      this.formatImage.style.display = "none";
+      this.imagePreview.style.display = "flex";
+    };
+    image.readAsDataURL(this.uploadInput.files[0]);
   }
-// Si il est valide Création d'un tableau avec la récupération des inputs du formulaire
-  const formData = new FormData();
-  formData.append("image", this.imageInput.files[0]);
-  formData.append("title", this.titleInput.value);
-  formData.append("category", this.categoryInput.value);
-
-  try {
-    const response = await fetch("http://localhost:5678/api/works", {
-      method: "POST",
-      headers: {
-        Authorization: "Bearer " + sessionStorage.getItem("token"),
-      },
-      body: formData,
-    });
-    const data = await response.json();
-    new WorksGallery()
-    this.imageInput.value = "";
-    this.imagePreview.style.display = "none";
-    this.faImg.style.display = "flex";
-    this.addPicture.style.display = "flex";
-    this.formatImage.style.display = "flex";
-  } catch (error) {
-    alert(error.message);
+  
+  // On vérifie la validité du formulaire et on change la couleur du bouton de validation de gris en vert si les données sont corrects
+  checkInputs() {
+    if (this.titleInput.value && this.categoryInput.value && this.imageInput.value) {
+      this.validateButton.style.backgroundColor = "#1d6154";
+      this.validateButton.style.border = "#1d6154";
+      this.validateButton.style.color = "white";
+    } else {
+      this.validateButton.style.backgroundColor = "grey";
+    }
   }
-}
-
-isFormValid() {
-  return this.titleInput.value && this.categoryInput.value && this.imageInput.value;
-}
-}
-
-// Crée une instance de la classe ImageUploadForm pour gérer le formulaire
-const imageForm = new ImageUploadForm();
-
+  
+  // Gérer la soumission du formulaire et vérifier que le formulaire est valide
+  async handleSubmit(event) {
+    event.preventDefault();
+    if (!this.isFormValid()) {
+      return;
+    }
+  // Si il est valide Création d'un tableau avec la récupération des inputs du formulaire
+    const formData = new FormData();
+    formData.append("image", this.imageInput.files[0]);
+    formData.append("title", this.titleInput.value);
+    formData.append("category", this.categoryInput.value);
+  
+    try {
+      const response = await fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + sessionStorage.getItem("token"),
+        },
+        body: formData,
+      });
+      const data = await response.json();
+      new WorksGallery()
+      this.imageInput.value = "";
+      this.imagePreview.style.display = "none";
+      this.faImg.style.display = "flex";
+      this.addPicture.style.display = "flex";
+      this.formatImage.style.display = "flex";
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+  
+  isFormValid() {
+    return this.titleInput.value && this.categoryInput.value && this.imageInput.value;
+  }
+  }
+  
+  // Crée une instance de la classe ImageUploadForm pour gérer le formulaire
+  const imageForm = new ImageUploadForm();
 
 })
 // Gestion de l'ouverture ou de la fermeture de la modale 
